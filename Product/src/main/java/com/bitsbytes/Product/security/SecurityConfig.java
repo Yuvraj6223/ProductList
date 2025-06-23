cd9c92c927007aefd3fb22c880a225f96ad243a6
Student Yuvraj <Student@yuvraj.in>
2025-06-23 12:35:58 +0530

    Final commit

--- Product/src/main/java/com/bitsbytes/Product/security/SecurityConfig.java
+++ Product/src/main/java/com/bitsbytes/Product/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         http.csrf(csrf->csrf.disable())
         .authorizeHttpRequests(request ->{
             request.requestMatchers( "/user/register", "/user/login").permitAll();
-            request.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
+            request.requestMatchers(HttpMethod.GET, "/**").permitAll();
             request.anyRequest().authenticated();
         })
         .oauth2ResourceServer(oauth2->oauth2.jwt(
